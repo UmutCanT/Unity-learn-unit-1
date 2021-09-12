7d9eb7d3da5c382640903044a18a07a251841722
UmutCanT <uctekcambusiness@gmail.com>
2021-09-12 16:33:57 +0300

    Game-physic-experiments

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    Rigidbody playerRb;
+
     [SerializeField]
     Camera[] cameras;
+
+    [SerializeField]
+    GameObject centerOfMass;
 
     [SerializeField]
     string playerID;
@@ -13,17 +18,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     KeyCode camSwitchKey;
 
+    [SerializeField]
+    float horsePower = 0f;
+
     float vehicleSpeed = 20f;
     float turnSpeed = 20f;
-
+   
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         VehicleController();
         CameraController();
@@ -34,7 +43,8 @@ public class PlayerController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal" + playerID);
         float verticalInput = Input.GetAxis("Vertical" + playerID);
 
-        transform.Translate(Vector3.forward * verticalInput * vehicleSpeed * Time.deltaTime);
+        //transform.Translate(Vector3.forward * verticalInput * vehicleSpeed * Time.deltaTime);
+        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
         transform.Rotate(Vector3.up * horizontalInput * turnSpeed * Time.deltaTime);
     }
 
